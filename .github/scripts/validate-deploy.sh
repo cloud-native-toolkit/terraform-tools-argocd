a17acf21eb1e25f3321bfcc9cc256b9c08897791
Sean Sundberg <seansund@us.ibm.com>
2020-06-13 18:26:37 -0500

    Validate endpoints in 'Validate deploy'

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+SCRIPT_DIR=$(cd $(dirname $0); pwd -P)
+
 CLUSTER_TYPE="$1"
 NAMESPACE="$2"
 
@@ -12,5 +14,17 @@ if [[ -n "${POD_STATUSES}" ]]; then
   echo "  Pods have non-success statuses: ${PODS}"
   exit 1
 fi
+
+if [[ "${CLUSTER_TYPE}" == "kubernetes" ]] || [[ "${CLUSTER_TYPE}" == "iks" ]]; then
+  ENDPOINTS=$(kubectl get ingress -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{range .spec.rules[*]}{"https://"}{.host}{"\n"}{end}{end}')
+else
+  ENDPOINTS=$(kubectl get route -n "${NAMESPACE}" -o jsonpath='{range .items[*]}{"https://"}{.spec.host}{"\n"}{end}')
+fi
+
+echo "Validating endpoints:\n${ENDPOINTS}"
+
+echo "${ENDPOINTS}" | while read endpoint; do
+  ${SCRIPT_DIR}/waitForEndpoint.sh "${endpoint}" 15 20
+done
 
 exit 0
