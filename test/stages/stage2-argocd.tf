8bfbaec04494b59ad43065fb6b5fa36b6e114fb4
Sean Sundberg <seansund@us.ibm.com>
2020-11-25 00:17:36 -0600

    Adds workflows and config for module catalog

--- test/stages/stage2-argocd.tf
+++ test/stages/stage2-argocd.tf
@@ -1,9 +1,9 @@
 module "dev_tools_argocd" {
   source = "./module"
 
-  cluster_config_file = module.dev_cluster.config_file_path
   cluster_type        = module.dev_cluster.type_code
   ingress_subdomain   = module.dev_cluster.ingress_hostname
+  cluster_config_file = module.dev_cluster.config_file_path
   olm_namespace       = module.dev_capture_olm_state.namespace
   operator_namespace  = module.dev_capture_operator_state.namespace
   app_namespace       = module.dev_capture_tools_state.namespace
