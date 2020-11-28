132a0c0160d47b6990e2c5ee0a09274049cee00f
Sean Sundberg <seansund@us.ibm.com>
2020-11-27 22:10:49 -0600

    Updates module merge logic

--- .github/scripts/merge-module-metadata.sh
+++ .github/scripts/merge-module-metadata.sh
@@ -3,7 +3,9 @@
 UPDATE_DIR="$1"
 
 if [[ -f "index.yaml" ]]; then
-  yq r index.yaml versions >> "${UPDATE_DIR}/module.yaml"
+  yq r index.yaml versions | yq p - versions > tmp.yaml
+  yq m -i -a "${UPDATE_DIR}/module.yaml" tmp.yaml
+  rm tmp.yaml
 fi
 
 cp "${UPDATE_DIR}/module.yaml" "${UPDATE_DIR}/index.yaml"
