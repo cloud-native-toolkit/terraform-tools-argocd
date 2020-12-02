abc1f439de06c81727cc8ca851985f2a53de7df4
Sean Sundberg <seansund@us.ibm.com>
2020-12-02 06:53:31 -0600

    Removes version number from namespace stage in test to get latest

--- test/stages/stage1-namespaces.tf
+++ test/stages/stage1-namespaces.tf
@@ -1,5 +1,5 @@
 module "dev_tools_namespace" {
-  source = "github.com/ibm-garage-cloud/terraform-cluster-namespace.git?ref=v2.1.0"
+  source = "github.com/ibm-garage-cloud/terraform-cluster-namespace.git"
 
   cluster_type             = module.dev_cluster.type_code
   cluster_config_file_path = module.dev_cluster.config_file_path
