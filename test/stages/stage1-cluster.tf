775fe95f4d7a089981f0b067f3f3b4e834fdbd89
Sean Sundberg <seansund@us.ibm.com>
2021-04-05 06:44:06 -0500

    Changes cluster module to ocp-vpc
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-cluster.tf
+++ test/stages/stage1-cluster.tf
@@ -1,18 +1,14 @@
 module "dev_cluster" {
-  source = "github.com/ibm-garage-cloud/terraform-ibm-container-platform.git"
+  source = "github.com/cloud-native-toolkit/terraform-ibm-ocp-vpc.git"
 
   resource_group_name     = var.resource_group_name
-  cluster_name            = var.cluster_name
-  cluster_region          = var.region
-  cluster_type            = substr(var.cluster_type, 0, 3) == "iks" ? "kubernetes" : var.cluster_type
-  cluster_exists          = true
+  region                  = var.region
   ibmcloud_api_key        = var.ibmcloud_api_key
+  name                    = var.cluster_name
+  worker_count            = 2
   name_prefix             = var.name_prefix
-  is_vpc                  = var.vpc_cluster
-  private_vlan_id         = ""
-  public_vlan_id          = ""
-  vlan_datacenter         = ""
-  cluster_machine_type    = ""
-  cluster_worker_count    = 3
-  cluster_hardware        = ""
+  exists                  = true
+  cos_id                  = ""
+  vpc_subnet_count        = 1
+  vpc_name                = ""
 }
