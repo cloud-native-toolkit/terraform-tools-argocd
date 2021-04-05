fdfa24aab4173ad3e3bd85b44358923dbd8df0e2
Sean Sundberg <seansund@us.ibm.com>
2021-04-05 06:50:08 -0500

    Fixes namespace test dependencies
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-namespaces.tf
+++ test/stages/stage1-namespaces.tf
@@ -1,8 +1,6 @@
 module "dev_tools_namespace" {
   source = "github.com/ibm-garage-cloud/terraform-cluster-namespace.git"
 
-  cluster_type             = module.dev_cluster.type_code
   cluster_config_file_path = module.dev_cluster.config_file_path
-  tls_secret_name          = module.dev_cluster.tls_secret_name
   name                     = var.namespace
 }
