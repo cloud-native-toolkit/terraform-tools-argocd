3145eed3cf73a0c1dc5f47fec346d75c2b245aeb
Sean Sundberg <seansund@us.ibm.com>
2021-04-05 08:26:18 -0500

    Adds ocp-vpc module to dependency list (#19)
    * Adds ocp-vpc module to dependency list
    * Fixes test dependencies
    * Adds logic to wait for all pods to start
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1b-capture-state.tf
+++ test/stages/stage1b-capture-state.tf
@@ -1,7 +1,7 @@
 module "dev_capture_tools_state" {
   source = "github.com/ibm-garage-cloud/terraform-k8s-capture-state"
 
-  cluster_type             = module.dev_cluster.type_code
+  cluster_type             = module.dev_cluster.platform.type_code
   cluster_config_file_path = module.dev_cluster.config_file_path
   namespace                = module.dev_tools_namespace.name
   output_path              = "${path.cwd}/cluster-state/before"
@@ -10,7 +10,7 @@ module "dev_capture_tools_state" {
 module "dev_capture_olm_state" {
   source = "github.com/ibm-garage-cloud/terraform-k8s-capture-state"
 
-  cluster_type             = module.dev_cluster.type_code
+  cluster_type             = module.dev_cluster.platform.type_code
   cluster_config_file_path = module.dev_cluster.config_file_path
   namespace                = module.dev_software_olm.olm_namespace
   output_path              = "${path.cwd}/cluster-state/before"
@@ -19,7 +19,7 @@ module "dev_capture_olm_state" {
 module "dev_capture_operator_state" {
   source = "github.com/ibm-garage-cloud/terraform-k8s-capture-state"
 
-  cluster_type             = module.dev_cluster.type_code
+  cluster_type             = module.dev_cluster.platform.type_code
   cluster_config_file_path = module.dev_cluster.config_file_path
   namespace                = module.dev_software_olm.target_namespace
   output_path              = "${path.cwd}/cluster-state/before"
---
+++ versions.tf
@@ -0,0 +1,8 @@
+terraform {
+  required_providers {
+    helm = {
+      version = ">= 1.1.1"
+    }
+  }
+  required_version = ">= 0.12"
+}
