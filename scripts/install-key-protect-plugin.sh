f6f5da5dbaa9216e5b6d4ab50eb44c67ab372d70
Sean Sundberg <seansund@us.ibm.com>
2021-05-31 13:41:48 -0500

    Passes name to install-plugin-dependencies.sh for key-protect plugin
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/install-key-protect-plugin.sh
+++ scripts/install-key-protect-plugin.sh
@@ -20,18 +20,18 @@ if ! command -v jq &> /dev/null; then
   export PATH=$PATH:/tmp/bin
 fi
 
-LATEST_RELEASE=$(curl -Ls https://api.github.com/repos/ibm-garage-cloud/argocd-plugin-key-protect/releases/latest | jq -r '.tag_name')
+LATEST_RELEASE=$(curl -Ls https://api.github.com/repos/cloud-native-toolkit/argocd-plugin-key-protect/releases/latest | jq -r '.tag_name')
 echo "Latest release of Key Protect plugin: ${LATEST_RELEASE}"
 
 echo "Downloading Key Protect plugin installer"
-curl -Lo ${TMP_DIR}/install-plugin.tar.gz "https://github.com/ibm-garage-cloud/argocd-plugin-key-protect/releases/download/${LATEST_RELEASE}/install-plugin.tar.gz"
+curl -Lo ${TMP_DIR}/install-plugin.tar.gz "https://github.com/cloud-native-toolkit/argocd-plugin-key-protect/releases/download/${LATEST_RELEASE}/install-plugin.tar.gz"
 
 echo "Extracting Key Protect plugin installer"
 mkdir -p "${TMP_DIR}/argocd/bin"
 cd "${TMP_DIR}/argocd/bin" && tar xzvf ${TMP_DIR}/install-plugin.tar.gz && cd -
 
 echo "Installing Key Protect plugin dependencies"
-${TMP_DIR}/argocd/bin/install-plugin-dependencies.sh "${NAMESPACE}"
+${TMP_DIR}/argocd/bin/install-plugin-dependencies.sh "${NAMESPACE}" "${NAME}"
 
 echo "Installing Key Protect plugin"
 ${TMP_DIR}/argocd/bin/install-plugin.sh "${NAMESPACE}" "${NAME}"
