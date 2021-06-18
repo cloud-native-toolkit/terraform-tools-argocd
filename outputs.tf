94f550080e2ef848637f4ce80cb3b7ed87997f11
Sean Sundberg <seansund@us.ibm.com>
2021-06-17 23:31:58 -0500

    Adds provision_tekton flag to outputs
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,9 @@ output "ingress_url" {
   value       = local.url_endpoint
   depends_on  = [helm_release.argocd-config]
 }
+
+output "provision_tekton" {
+  description = "Flag indicating that Tekton should be provisioned"
+  value       = !local.openshift_gitops
+  depends_on  = [helm_release.argocd-config]
+}
