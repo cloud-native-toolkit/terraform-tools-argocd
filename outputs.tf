7f0d007110c9e351d020cd33bfde36555418178f
Sean Sundberg <seansund@us.ibm.com>
2021-06-20 14:32:25 -0500

    Adds sensitive flag for host and url for some reason
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,14 @@ output "ingress_host" {
   description = "The ingress host for the Argo CD instance"
   value       = local.host
   depends_on  = [helm_release.argocd-config]
+  sensitive   = true
 }
 
 output "ingress_url" {
   description = "The ingress url for the Argo CD instance"
   value       = local.url_endpoint
   depends_on  = [helm_release.argocd-config]
+  sensitive   = true
 }
 
 output "provision_tekton" {
