ffb12d632fc2bc5590dbe4ecf2429ac3346fa30a
Sean Sundberg <seansund@us.ibm.com>
2021-06-21 09:40:26 -0500

    Adds login to test host with auth token
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- .github/scripts/validate-deploy.sh
+++ .github/scripts/validate-deploy.sh
@@ -87,6 +87,12 @@ ${ARGOCD} login "${ARGO_HOST}" --username "${ARGO_USERNAME}" --password "${ARGO_
 echo "Logging in to argocd: ${TEST_HOST} ${ARGO_PASSWORD}"
 ${ARGOCD} login "${TEST_HOST}" --username "${ARGO_USERNAME}" --password "${ARGO_PASSWORD}" --insecure --grpc-web --loglevel debug
 
+TOKEN=$(oc whoami -t)
+
+echo "Logging in to argocd: ${TEST_HOST} token"
+${ARGOCD} login "${TEST_HOST}" --auth-token "${TOKEN}" --insecure --grpc-web --loglevel debug
+
+
 oc get pods -n "${NAMESPACE}"
 
 echo "Validating argocd-access secret"
