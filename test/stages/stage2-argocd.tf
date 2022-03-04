a8634f39a33218864e928bcbca03078f0e01b989
Sean Sundberg <seansund@us.ibm.com>
2022-03-03 22:33:22 -0600

    Updates test variables
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage2-argocd.tf
+++ test/stages/stage2-argocd.tf
@@ -3,7 +3,6 @@ module "dev_tools_argocd" {
 
   cluster_config_file = module.dev_cluster.config_file_path
   olm_namespace       = module.dev_capture_olm_state.namespace
-  operator_namespace  = module.dev_capture_operator_state.namespace
   app_namespace       = module.dev_capture_tools_state.namespace
   name                = "argocd"
 }
