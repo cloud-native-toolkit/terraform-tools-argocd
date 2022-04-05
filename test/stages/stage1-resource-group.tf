74fd75ded6ee74936431ed2363eeee1692f59d95
Sean Sundberg <seansund@us.ibm.com>
2022-04-05 14:48:59 -0500

    Updates error handling and reporting for wait-for-statefulset (#63)
    closes #62
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- test/stages/stage1-resource-group.tf
+++ test/stages/stage1-resource-group.tf
@@ -2,5 +2,5 @@ module "resource_group" {
   source = "github.com/cloud-native-toolkit/terraform-ibm-resource-group.git"
 
   resource_group_name = var.resource_group_name
-  provision           = false
+  ibmcloud_api_key    = var.ibmcloud_api_key
 }
--- test/stages/stage2-argocd.tf
+++ test/stages/stage2-argocd.tf
@@ -2,15 +2,15 @@ module "dev_tools_argocd" {
   source = "./module"
 
   cluster_config_file = module.dev_cluster.config_file_path
-  olm_namespace       = module.dev_capture_olm_state.namespace
-  operator_namespace  = module.dev_capture_operator_state.namespace
-  app_namespace       = module.dev_capture_tools_state.namespace
+  olm_namespace       = module.dev_software_olm.olm_namespace
+  operator_namespace  = module.dev_software_olm.target_namespace
+  app_namespace       = module.dev_tools_namespace.name
   name                = "argocd"
 }
 
 resource "null_resource" "output_values" {
   provisioner "local-exec" {
-    command = "echo -n '${module.dev_capture_tools_state.namespace}' > .namespace"
+    command = "echo -n '${module.dev_tools_argocd.namespace}' > .namespace"
   }
   provisioner "local-exec" {
     command = "echo -n '${module.dev_tools_argocd.namespace}' > .argo-namespace"
