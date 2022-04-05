cb6b44661d4dab84304ed8d48a1d921c2c072f0e
Sean Sundberg <seansund@us.ibm.com>
2022-04-05 08:22:18 -0500

    Updates error handling and reporting for wait-for-statefulset
    closes #62
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/wait-for-statefulset.sh
+++ scripts/wait-for-statefulset.sh
@@ -2,8 +2,28 @@
 
 NAMESPACE="$1"
 
-KUBECTL="${BIN_DIR}/kubectl"
+export PATH="${BIN_DIR}:${PATH}"
 
-${KUBECTL} get statefulset -n "${NAMESPACE}" -l app.kubernetes.io/part-of=argocd -o jsonpath='{range .items[]}{.metadata.name}{"\n"}{end}' | while read name; do
-  ${KUBECTL} rollout status "statefulset/${name}"
+LABEL="app.kubernetes.io/part-of=argocd"
+
+count=0
+until [[ $(kubectl get statefulset -n "${NAMESPACE}" -l "${LABEL}" -o json | jq '.items | length') -gt 0 ]] || [[ $count -gt 20 ]]; do
+  echo "Waiting for StatefulSet in ${NAMESPACE} with label ${LABEL}"
+  count=$((count + 1))
+  sleep 30
 done
+
+if [[ $count -gt 20 ]]; then
+  echo "Timed out waiting for StatefulSet in ${NAMESPACE} with label ${LABEL}" >&2
+  kubectl get statefulset -n "${NAMESPACE}" --show-labels
+  exit 1
+fi
+
+name=$(kubectl get statefulset -n "${NAMESPACE}" -l "${LABEL}" -o json | jq -r '.items[0].metadata.name')
+
+if [[ -z "${name}" ]]; then
+  echo "StatefulSet name not found" >&2
+  exit 1
+fi
+
+kubectl rollout status -n "${NAMESPACE}" "statefulset/${name}"
