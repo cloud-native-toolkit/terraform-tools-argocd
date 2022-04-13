411f4ecd2fe32deb201d730fbbf2edebe4989755
Sean Sundberg <seansund@us.ibm.com>
2022-04-13 00:39:59 -0500

    Updates helm deploy and destroy logic
    closes #69
    
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/destroy-helm.sh
+++ scripts/destroy-helm.sh
@@ -9,31 +9,17 @@ if [[ -z "${TMP_DIR}" ]]; then
 fi
 mkdir -p "${TMP_DIR}"
 
-if [[ -z "${BIN_DIR}" ]]; then
-  mkdir -p ./bin
-  BIN_DIR=$(cd ./bin; pwd -P)
+if [[ -n "${BIN_DIR}" ]]; then
+  export PATH="${BIN_DIR}:${PATH}"
 fi
 
 VALUES_FILE="${TMP_DIR}/${NAME}-values.yaml"
 
 echo "${VALUES_FILE_CONTENT}" > "${VALUES_FILE}"
 
-HELM=$(command -v "${BIN_DIR}/helm" || command -v helm)
-if [[ -z "${HELM}" ]]; then
-  echo "helm cli missing"
+if ! command -v helm 1> /dev/null 2> /dev/null; then
+  echo "helm cli missing" >&2
   exit 1
 fi
 
-KUBECTL=$(command -v "${BIN_DIR}/kubectl" || command -v kubectl)
-if [[ -z "${KUBECTL}" ]]; then
-  echo "kubectl cli missing"
-  exit 1
-fi
-
-${KUBECTL} config set-context --current --namespace "${NAMESPACE}"
-
-if [[ -n "${REPO}" ]]; then
-  repo_config="--repo ${REPO}"
-fi
-
-${HELM} template "${NAME}" "${CHART}" ${repo_config} --values "${VALUES_FILE}" | ${KUBECTL} delete -f -
+helm delete "${NAME}" -n "${NAMESPACE}" || echo "Unable to find helm release: ${NAME}"
