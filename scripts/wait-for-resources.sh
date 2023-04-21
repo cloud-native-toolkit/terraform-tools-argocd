2081d7821cc3e680595991e02bb1ef7016b2fe24
Sean Sundberg <seansund@us.ibm.com>
2023-04-20 22:38:34 -0500

    Increase timeout waiting for resources
    Signed-off-by: Sean Sundberg <seansund@us.ibm.com>

--- scripts/wait-for-resources.sh
+++ scripts/wait-for-resources.sh
@@ -8,11 +8,11 @@ export PATH="${BIN_DIR}:${PATH}"
 sleep 30
 
 kubectl get statefulset,deployment -n "${NAMESPACE}" -l "${LABEL}" -o json | jq -c '.items[] | {"kind": .kind, "name": .metadata.name}' | while read resource; do
-  kind=$(echo "${resource}" | jq -r '.kind')
+  kind=$(echo "${resource}" | jq -r '.kind' | tr '[:upper:]' '[:lower:]')
   name=$(echo "${resource}" | jq -r '.name')
 
-  echo "Waiting for ${kind}/${name} in ${NAMESPACE}"
-  kubectl rollout status -n "${NAMESPACE}" "${kind}/${name}" --timeout=1h || exit 1
+  echo "Waiting for ${kind}/${name} in ${NAMESPACE} with 2h timeout"
+  kubectl rollout status -n "${NAMESPACE}" "${kind}/${name}" --timeout=2h || exit 1
 done
 
 # do it again in case there are new ones
